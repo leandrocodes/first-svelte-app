a8cd284297566f96c32778becf1bfb24f4cef0b5
leandrocodes <leandrovianacodes@gmail.com>
2020-05-26 08:32:37 -0400

    nice navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,9 +1,19 @@
 <script>
-  export let navItems = ['Navbar', 'Home', 'Projects', 'Setup', 'Contact']
+  export let navItems = ['Home', 'Projects', 'Setup', 'Contact']
 </script>
 
 <style>
-
+  ul {
+    display: flex;
+    list-style-type: none;
+    justify-content: flex-end;
+  }
+  ul li {
+    margin: 0 15px;
+  }
+  ul li:hover {
+    color: #f53e03;
+  }
 </style>
 
 <main>
