2c1f7d871ca48a5cb611e193a3f257fdd177d5c8
leandrocodes <leandrovianacodes@gmail.com>
2020-05-26 08:28:59 -0400

    simple navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
-	export let name
+  import Navbar from './components/navbar.svelte'
+  export let name
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: #f53e03;
+    text-align: center;
+    font-weight: 100;
+    text-transform: uppercase;
+  }
 </style>
 
+<Navbar />
 <h1>Hello {name}!</h1>
---
+++ src/components/navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let navItems = ['Navbar', 'Home', 'Projects', 'Setup', 'Contact']
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <ul>
+    {#each navItems as navItem}
+      <li>{navItem}</li>
+    {/each}
+  </ul>
+</main>
